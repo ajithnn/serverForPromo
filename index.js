09dc85b50d79fb623b2ffc688ea44d8015260194
ajithnn <ajithnn20@gmail.com>
2015-08-14 17:27:56 +0530

    latest code

--- index.js
+++ index.js
@@ -21,27 +21,27 @@ rest.setHeaders(header);
 
 rest.getfile('/image', function(err, query) {
     if (!err) {
-        fs.readFile('/app/assets/out.txt', 'utf8', function (err,data) {
-                if (err) {
-                        return console.log(err);
-                }
-                       return data;
-                });
+        fs.readFile('/app/assets/out.txt', 'utf8', function(err, data) {
+            if (err) {
+                return console.log(err);
+            }
+            return data;
+        });
     } else {
         console.log(err);
         return err;
     }
 })
 
-rest.get('/runpy', function(err, query,ctype) {
+rest.get('/runpy', function(err, query, ctype) {
     var statFlag = true;
     if (!err) {
         var pathforPython = 'python ';
-        var pathForFile = __dirname + '/python/grayFaceGreenEye.py ';// Change here the python file name.
-        console.log(pathforPython + pathForFile + " /app/assets/outImage_"+ query.id + ".txt");// change the params below also.
-        exec(pathforPython + pathForFile + " /app/assets/outImage_"+ query.id + ".txt", function(error, stdout, stderr) {
-             console.log(stdout);  
-             console.log(error); 
+        var pathForFile = __dirname + '/python/grayFaceGreenEye.py '; // Change here the python file name.
+        console.log(pathforPython + pathForFile + " /app/assets/outImage_" + query.id + ".txt"); // change the params below also.
+        exec(pathforPython + pathForFile + " /app/assets/outImage_" + query.id + ".txt", function(error, stdout, stderr) {
+            console.log(stdout);
+            console.log(error);
         });
         return "done"
     } else {
@@ -62,25 +62,28 @@ rest.multipost('/PostPhoto', function(err, data) {
 var io = rest.getSocketServer()
 
 io.on('connection', function(socket) {
-	console.log("Connected: " + socket.id);
+    console.log("Connected: " + socket.id);
     fs.watch('./assets/', function(event, filename) {
-        if(filename == "outImage_"+socket.id + ".txt")
-        {
-        var time = new Date().getTime();
-        console.log("Sent");
-        io.to(socket.id).emit("OpenUrl", "/image?id=outImage_" + socket.id + "&time=" + time);
+        if (filename == "out.txt") {
+            var time = new Date().getTime();
+            console.log("Sent");
+            fs.readFile('/app/assets/out.txt', 'utf8', function(err, data) {
+                if (err) {
+                    return console.log(err);
+                }
+                io.to(socket.id).emit("OpenUrl", data);
+            });
+        } else if (filename == "error_" + socket.id) {
+            fs.readFile(filename, 'utf8', function(err, data) {
+                if (err) {
+                    return console.log(err);
+                }
+                io.to(socket.id).emit("Error", data);
+            });
         }
-	else if(filename == "error_" + socket.id){
-		fs.readFile(filename, 'utf8', function (err,data) {
- 	 	if (err) {
-    			return console.log(err);
-  		}
-  			io.to(socket.id).emit("Error",data);
-		});
-	}
     });
 });
-rest.port = process.env.PORT || 3000 ;
+rest.port = process.env.PORT || 3000;
 rest.listen(process.env.PORT || 3000, function() {
     console.log("Listening on port 0.0.0.0:%s", process.env.PORT || rest.port)
 })
